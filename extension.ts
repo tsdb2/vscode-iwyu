738264f8a03872cdcd3261459f9265d9c25f7e64
lord-pando <alberto@tsdb2.com>
2024-12-10 22:51:43 +0100

    Suggest quick fixes for removing unneeded headers.

--- extension.ts
+++ extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import child from 'node:child_process';
 import path from 'node:path';
 
+const supportedLanguages = ['cpp', 'c'];
+
 function exec(command: string, options: object): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     child.exec(command, options, (error, stdout, stderr) => {
@@ -12,6 +14,36 @@ function exec(command: string, options: object): Promise<string> {
       }
     });
   });
+}
+
+class CodeActionProvider implements vscode.CodeActionProvider {
+  public provideCodeActions(
+    document: vscode.TextDocument,
+    range: vscode.Range | vscode.Selection,
+    context: vscode.CodeActionContext,
+    token: vscode.CancellationToken,
+  ): vscode.ProviderResult<vscode.CodeAction[]> {
+    return context.diagnostics
+      .filter(diagnostic => diagnostic.source === 'IWYU' && diagnostic.code === 'remove')
+      .map(diagnostic => CodeActionProvider.createQuickFix(document, diagnostic));
+  }
+
+  private static createQuickFix(
+    document: vscode.TextDocument,
+    diagnostic: vscode.Diagnostic,
+  ): vscode.CodeAction {
+    const match = diagnostic.message.match(/^Unused #include (.+)\.$/);
+    const action = new vscode.CodeAction(
+      match ? `Remove header ${match[1]}` : 'Remove header',
+      vscode.CodeActionKind.QuickFix,
+    );
+    action.diagnostics = [diagnostic];
+    action.edit = new vscode.WorkspaceEdit();
+    const start = diagnostic.range.start;
+    const range = new vscode.Range(start.line, start.character, start.line + 1, 0);
+    action.edit.delete(document.uri, range);
+    return action;
+  }
 }
 
 function processAddFinding(
@@ -31,13 +63,14 @@ function processAddFinding(
   }
   const lineNumber = parseInt(row, 10) - 1;
   const charNumber = parseInt(column, 10) - 1;
-  diagnostics.push(
-    new vscode.Diagnostic(
-      new vscode.Range(lineNumber, charNumber, lineNumber, charNumber + symbol.length),
-      `${symbol} is defined in ${include}, which isn't directly #included.`,
-      vscode.DiagnosticSeverity.Warning,
-    ),
+  const diagnostic = new vscode.Diagnostic(
+    new vscode.Range(lineNumber, charNumber, lineNumber, charNumber + 1),
+    `${symbol} is defined in ${include}, which isn't directly #included.`,
+    vscode.DiagnosticSeverity.Warning,
   );
+  diagnostic.source = 'IWYU';
+  diagnostic.code = 'add';
+  diagnostics.push(diagnostic);
   return true;
 }
 
@@ -51,13 +84,14 @@ function processRemoveFinding(diagnostics: vscode.Diagnostic[], line: string): v
   if (match) {
     const [, include, includePath, row] = match;
     const lineNumber = parseInt(row, 10) - 1;
-    diagnostics.push(
-      new vscode.Diagnostic(
-        new vscode.Range(lineNumber, 0, lineNumber, include.length),
-        `Unused #include ${includePath}.`,
-        vscode.DiagnosticSeverity.Hint,
-      ),
+    const diagnostic = new vscode.Diagnostic(
+      new vscode.Range(lineNumber, 0, lineNumber, include.length),
+      `Unused #include ${includePath}.`,
+      vscode.DiagnosticSeverity.Warning,
     );
+    diagnostic.source = 'IWYU';
+    diagnostic.code = 'remove';
+    diagnostics.push(diagnostic);
   }
 }
 
@@ -91,7 +125,10 @@ async function runIWYU(document: vscode.TextDocument): Promise<void> {
     throw Error(`'${activeFileUri.fsPath}' doesn't seem to be located in an open workspace.`);
   }
   const commandFilePath = path.join(workspaceRoot, 'compile_commands.json');
-  const sourceFilePath = path.relative(workspaceRoot, activeFileUri.fsPath);
+  const sourceFilePath = vscode.workspace.asRelativePath(
+    activeFileUri,
+    /*includeWorkspaceFolder=*/ false,
+  );
   const command = `iwyu_tool -p '${commandFilePath}' '${sourceFilePath}' -- -Xiwyu --no_fwd_decls -Xiwyu --verbose=3 -Xiwyu --cxx17ns`;
   try {
     const stdout = await exec(command, { cwd: workspaceRoot });
@@ -104,17 +141,23 @@ async function runIWYU(document: vscode.TextDocument): Promise<void> {
 
 let timeout: NodeJS.Timeout | null = null;
 
-function scheduleIWYU(document: vscode.TextDocument): void {
+function scheduleIWYU(document: vscode.TextDocument, inSeconds: number): void {
   if (timeout !== null) {
     clearTimeout(timeout);
   }
   timeout = setTimeout(() => {
     timeout = null;
     runIWYU(document);
-  }, 10000);
+  }, inSeconds * 1000);
 }
 
 export function activate(context: vscode.ExtensionContext): void {
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider(
+      supportedLanguages.map(language => ({ language })),
+      new CodeActionProvider(),
+    ),
+  );
   context.subscriptions.push(
     vscode.commands.registerCommand('iwyu.analyzeFile', () => {
       const editor = vscode.window.activeTextEditor;
@@ -126,9 +169,16 @@ export function activate(context: vscode.ExtensionContext): void {
     }),
   );
   context.subscriptions.push(
+    vscode.workspace.onDidOpenTextDocument(document => {
+      if (supportedLanguages.includes(document.languageId)) {
+        runIWYU(document);
+      }
+    }),
+  );
+  context.subscriptions.push(
     vscode.workspace.onDidSaveTextDocument(document => {
-      if (document.languageId === 'cpp') {
-        scheduleIWYU(document);
+      if (supportedLanguages.includes(document.languageId)) {
+        scheduleIWYU(document, 5);
       }
     }),
   );
