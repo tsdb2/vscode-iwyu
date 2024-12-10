a11e10c1d763d4e55848337bc7e71728308b1312
lord-pando <alberto@tsdb2.com>
2024-12-10 17:52:25 +0100

    Add functionality.

--- extension.ts
+++ extension.ts
@@ -14,25 +14,66 @@ function exec(command: string, options: object): Promise<string> {
   });
 }
 
-function processOutput(document: vscode.TextDocument, output: string): void {
+function processAddFinding(
+  diagnostics: vscode.Diagnostic[],
+  line: string,
+  sourceFilePath: string,
+): boolean {
+  const match = line.match(
+    /^([^:]+):(\d+):(\d+): warning: ([^\s]+) is defined in ("[^"]+"|<[^>]+>), which isn't directly #included.$/,
+  );
+  if (!match || match[1] !== sourceFilePath) {
+    return false;
+  }
+  const [, path, row, column, symbol, include] = match;
+  if (path !== sourceFilePath) {
+    return false;
+  }
+  const lineNumber = parseInt(row, 10) - 1;
+  const charNumber = parseInt(column, 10) - 1;
+  diagnostics.push(
+    new vscode.Diagnostic(
+      new vscode.Range(lineNumber, charNumber, lineNumber, charNumber + symbol.length),
+      `${symbol} is defined in ${include}, which isn't directly #included.`,
+      vscode.DiagnosticSeverity.Warning,
+    ),
+  );
+  return true;
+}
+
+function isRemoveFinding(line: string, sourceFilePath: string): boolean {
+  const match = line.match(/^(.*) should remove these lines:$/);
+  return match !== null && match[1] === sourceFilePath;
+}
+
+function processRemoveFinding(diagnostics: vscode.Diagnostic[], line: string): void {
+  const match = line.match(/^- (#include\s*("[^"]+"|<[^>]+>))\s*\/\/ lines (\d+)-(\d+)/);
+  if (match) {
+    const [, include, includePath, row] = match;
+    const lineNumber = parseInt(row, 10) - 1;
+    diagnostics.push(
+      new vscode.Diagnostic(
+        new vscode.Range(lineNumber, 0, lineNumber, include.length),
+        `Unused #include ${includePath}.`,
+        vscode.DiagnosticSeverity.Hint,
+      ),
+    );
+  }
+}
+
+function processOutput(
+  document: vscode.TextDocument,
+  sourceFilePath: string,
+  output: string,
+): void {
   const diagnostics: vscode.Diagnostic[] = [];
   const lines = output.split('\n');
   for (let i = 0; i < lines.length; i++) {
-    const match = /^(.*) should remove these lines:$/.exec(lines[i]);
-    if (!match) {
-      continue;
-    }
-    if (match[1] !== document.fileName) { continue; }
-    for (i++; i < lines.length && lines[i]; i++) {
-      const match = lines[i].match(/^- #include\s*(?:"[^"]+"|<[^>]+>)\s*\/\/ lines (\d+)-(\d+)/);
-      if (match) {
-        const [, startLine, endLine] = match;
-        diagnostics.push({
-          range: new vscode.Range(parseInt(startLine, 10) - 1, 0, parseInt(endLine, 10), 0),
-          message: 'Unused #include.',
-          severity: vscode.DiagnosticSeverity.Hint,
-          source: 'IWYU',
-        });
+    if (!processAddFinding(diagnostics, lines[i], sourceFilePath)) {
+      if (isRemoveFinding(lines[i], sourceFilePath)) {
+        for (i++; i < lines.length && lines[i] !== ''; i++) {
+          processRemoveFinding(diagnostics, lines[i]);
+        }
       }
     }
   }
@@ -51,11 +92,11 @@ async function runIWYU(document: vscode.TextDocument): Promise<void> {
   }
   const commandFilePath = path.join(workspaceRoot, 'compile_commands.json');
   const sourceFilePath = path.relative(workspaceRoot, activeFileUri.fsPath);
-  const command = `iwyu_tool -p '${commandFilePath}' '${sourceFilePath}' -- -Xiwyu --no_fwd_decls`;
+  const command = `iwyu_tool -p '${commandFilePath}' '${sourceFilePath}' -- -Xiwyu --no_fwd_decls -Xiwyu --verbose=3 -Xiwyu --cxx17ns`;
   try {
     const stdout = await exec(command, { cwd: workspaceRoot });
-    console.info(`IWYU run:\n$ ${command}\n${stdout}`);
-    processOutput(document, stdout);
+    // console.info(`IWYU run:\n$ ${command}\n${stdout}`);
+    processOutput(document, sourceFilePath, stdout);
   } catch (error) {
     vscode.window.showErrorMessage(`IWYU error: ${error}`);
   }
@@ -74,19 +115,23 @@ function scheduleIWYU(document: vscode.TextDocument): void {
 }
 
 export function activate(context: vscode.ExtensionContext): void {
-  context.subscriptions.push(vscode.commands.registerCommand('iwyu.analyzeFile', () => {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      runIWYU(editor.document);
-    } else {
-      vscode.window.showErrorMessage('No file is currently open to analyze.');
-    }
-  }));
-  context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(document => {
-    if (document.languageId === 'cpp') {
-      scheduleIWYU(document);
-    }
-  }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('iwyu.analyzeFile', () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        runIWYU(editor.document);
+      } else {
+        vscode.window.showErrorMessage('No file is currently open to analyze.');
+      }
+    }),
+  );
+  context.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument(document => {
+      if (document.languageId === 'cpp') {
+        scheduleIWYU(document);
+      }
+    }),
+  );
 }
 
-export function deactivate(): void { }
+export function deactivate(): void {}
